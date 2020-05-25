b44333a8a50305c55d0a4e0c8a1e2f33425e17b2
Nakayama Reiya <nkymriy@gmail.com>
2020-05-26 05:52:25 +0900

    HelloWorldを設置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
+# class ApplicationController < ActionController::Base
+# end
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  def hello
+    render html: "Hello Ruby on rails world !!!!"
+  end
 end
